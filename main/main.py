335ee0be14ef54e35f032fd1779c9d27c9b3b823
Victoriya  Fateeva <fvika2002@gmail.com>
2024-10-27 13:41:13 +0500

    Выполнено дз

---
+++ main/main.py
@@ -0,0 +1,10 @@
+from modules import mod1
+from  modules import mod2
+from package.papka1 import f1
+from package.papka1 import f2
+from package.papka1.papka2 import f3
+from package.papka1.papka2 import f4
+from package.papka3 import f5
+from package.papka3 import f6
+
+
